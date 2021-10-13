b6cdf7b92a9dcdeaccb4386d593d88a1902a1ed4
beaderiquito <beaderiquito@gmail.com>
2021-10-13 21:03:27 +0800

    added a really rough navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbNavConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [NgbNavConfig]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(config: NgbNavConfig) {
+    //customize default values of navs used by this component tree
+    config.destroyOnHide = false;
+    config.roles = false;
+   }
 
 }
