05986f8ef072aaeb6686dfe53ff925348ea82985
beaderiquito <beaderiquito@gmail.com>
2021-10-13 13:11:41 +0800

    subscribed to ProductService

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  //To close list of products:
+  //closeProduct: boolean = false;
 
-  constructor() { }
+  selectedProduct?: Product;
+  products: Product[] = [];
+
+  constructor(
+    public productService: ProductService,
+    public router: Router,
+  ){}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  onSelect(product: Product): void{
+    this.selectedProduct = product;
+    //this.closeProduct = true;
+  }
+
+  getProducts(): void {
+    this.productService.getProducts()
+      .subscribe(products => this.products = products);
+  }
 }
