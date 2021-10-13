c11f906bf95893c10017dd40611e779878730e96
beaderiquito <beaderiquito@gmail.com>
2021-10-13 12:59:03 +0800

    created ProductService

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from './Product';
+import { PRODUCTS } from './mock-products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  getProducts(): Observable<Product []> {
+    const products = of(PRODUCTS);
+    return products;
+  }
+  getProduct(id: number): Observable<Product>{
+    const product = PRODUCTS.find(p => p.id === id)!;
+    return of(product);
+  }
+}
