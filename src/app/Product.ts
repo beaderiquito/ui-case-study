cc4d2f2a4dc0279e015c30e08b00d15ed92bcd69
beaderiquito <beaderiquito@gmail.com>
2021-10-13 12:10:49 +0800

    created new class Product

---
+++ src/app/Product.ts
@@ -0,0 +1,18 @@
+import { Map } from "typescript";
+
+export class Product{
+    id: number;
+    photo: string;
+    name: string;
+    price: number;
+    reviews: any;
+
+    constructor(id: number, photo: string, name:string, price:number, reviews:any){
+        this.id = id;
+        this.photo = photo;
+        this.name = name;
+        this.price = price;
+        this.reviews = reviews;
+    }
+
+}
