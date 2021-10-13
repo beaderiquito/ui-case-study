4b1d67e765e15646a241367662c13480347b6025
beaderiquito <beaderiquito@gmail.com>
2021-10-13 23:54:22 +0800

    fixed reviews in mock-products.ts

--- src/app/mock-products.ts
+++ src/app/mock-products.ts
@@ -3,43 +3,43 @@ import { Product } from "./Product";
 export const PRODUCTS: Product[] = [
     {id: 1, photo: 'https://image.freepik.com/free-psd/arrangement-colorful-mock-up-doypack_23-2149015936.jpg', name: 'Product 1', price: 1000, reviews: 
         [
-            {username: 'username1', date: 'April 20, 2021', review: 'Excellent!', rating: 5},
-            {username: 'username2', date: 'April 20, 2021', review: 'Ok quality.', rating: 3},
-            {username: 'username3', date: 'April 20, 2021', review: 'Do not buy!', rating: 1},    
+            {username: 'username1', date: 'April 20, 2021', content: 'Excellent!', stars: 5},
+            {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
+            {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
         ]
     },
     {id: 2, photo: 'https://image.freepik.com/free-psd/arrangement-colorful-mock-up-doypack_23-2149015936.jpg', name: 'Product 2', price: 2000, reviews:  
         [
-            {username: 'username1', date: 'April 20, 2021', review: 'Excellent!', rating: 5},
-            {username: 'username2', date: 'April 20, 2021', review: 'Ok quality.', rating: 3},
-            {username: 'username3', date: 'April 20, 2021', review: 'Do not buy!', rating: 1},    
+            {username: 'username1', date: 'April 20, 2021', content: 'Excellent!', stars: 5},
+            {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
+            {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
         ]
     },
     {id: 3, photo: 'https://image.freepik.com/free-psd/arrangement-colorful-mock-up-doypack_23-2149015936.jpg', name: 'Product 3', price: 3000, reviews:  
         [
-            {username: 'username1', date: 'April 20, 2021', review: 'Excellent!', rating: 5},
-            {username: 'username2', date: 'April 20, 2021', review: 'Ok quality.', rating: 3},
-            {username: 'username3', date: 'April 20, 2021', review: 'Do not buy!', rating: 1},    
+            {username: 'username1', date: 'April 20, 2021', content: 'Excellent!', stars: 5},
+            {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
+            {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
         ]
     },
     {id: 4, photo: 'https://image.freepik.com/free-psd/arrangement-colorful-mock-up-doypack_23-2149015936.jpg', name: 'Product 4', price: 4000, reviews:  
         [
-            {username: 'username1', date: 'April 20, 2021', review: 'Excellent!', rating: 5},
-            {username: 'username2', date: 'April 20, 2021', review: 'Ok quality.', rating: 3},
-            {username: 'username3', date: 'April 20, 2021', review: 'Do not buy!', rating: 1},    
+            {username: 'username1', date: 'April 20, 2021', content: 'Excellent!', stars: 5},
+            {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
+            {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
         ]
     },
     {id: 5, photo: 'https://image.freepik.com/free-psd/arrangement-colorful-mock-up-doypack_23-2149015936.jpg', name: 'Product 5', price: 5000, reviews:  
         [
-            {username: 'username1', date: 'April 20, 2021', review: 'Excellent!', rating: 5},
-            {username: 'username2', date: 'April 20, 2021', review: 'Ok quality.', rating: 3},
-            {username: 'username3', date: 'April 20, 2021', review: 'Do not buy!', rating: 1},    
+            {username: 'username1', date: 'April 20, 2021', content: 'Excellent!', stars: 5},
+            {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
+            {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
         ]
     },
     {id: 6, photo: 'https://image.freepik.com/free-psd/arrangement-colorful-mock-up-doypack_23-2149015936.jpg', name: 'Product 6', price: 6000, reviews:  
         [
-            {username: 'username1', date: 'April 20, 2021', review: 'Excellent!', rating: 5},
-            {username: 'username2', date: 'April 20, 2021', review: 'Ok quality.', rating: 3},
-            {username: 'username3', date: 'April 20, 2021', review: 'Do not buy!', rating: 1},    
+            {username: 'username1', date: 'April 20, 2021', content: 'Excellent!', stars: 5},
+            {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
+            {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
         ]
     }]
