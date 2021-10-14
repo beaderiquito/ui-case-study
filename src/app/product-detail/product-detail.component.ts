47ba6a1ed7f5fefe8a2361bef515a1235a83cbc1
prankoi <batuken.manao@gmail.com>
2021-10-14 20:26:09 +0800

    added display and temporary template for product-details page

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,12 +10,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   id: string | null = '';
+  productId: number = 0;
+  products: Product[] = [];
+  product: any;
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
     this._route.paramMap.subscribe(params => {
       this.id = params.get('product-id');
     });
+
+    this.productId = Number(this.id);
+    this.getProductDetails();
+  }
+
+  getAllProducts(): void {
+    this.productService.getAllProducts()
+      .subscribe(products => this.products = products);
+    this.products = this.products;
+  }
+
+  getProductDetails(): void {
+    this.productService.getProduct(this.productId)
+      .subscribe(product => this.product = product);
+    this.product = this.product;
+  }
+
+  getAverageStars(array: any){
+    let average: number = 0;
+    for(let i=0; i<array.length; i++){
+      average = (average + array[i].stars) / (i+1);
+    }
+    return Math.ceil(average);
   }
 }
