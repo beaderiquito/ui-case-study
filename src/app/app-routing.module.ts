16c87f2f6a45844e38f9fd529a5b1e265a9efdae
Gerald Avila <iamgeraldavila@gmail.com>
2021-10-14 09:43:00 +0800

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent, pathMatch: 'full'},
+  {path: 'product-list', component: ProductListComponent},
+  {path: 'product-details', component: ProductDetailComponent},
+  {path: 'cart', component: CartComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
