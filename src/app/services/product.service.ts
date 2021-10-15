fc44569f3e0f5d7f3f3d2089c7da202b505e70c7
rc-namoco <reyanna.namoco@pointwest.com.ph>
2021-10-15 14:09:52 +0800

    updated product descriptions in product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,7 +12,7 @@ export class ProductService {
           {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
           {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
       ],
-      description: 'Product description'
+      description: 'Our croissant is a buttery, crescent-shaped French pastry. Enjoy this light, flaky, and delicately sweet croissant freshly made everyday. Enjoy one with a cup of coffee — to boost your productivity while enjoying this delicious treat.'
   },
   {id: 2, photo: '../assets/img/products/apple-danish.jpg', name: 'Apple Danish', price: 125, reviews:  
       [
@@ -20,7 +20,7 @@ export class ProductService {
           {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 2},
           {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 4},    
       ],
-      description: 'Product description'
+      description: 'Apple Danishes are the perfect way to welcome the cold season! The flaky crust, apple pie filling, crumb topping, and simple glaze make them irresistible. They make for a perfect snack but are also great for your cheat days.'
   },
   {id: 3, photo: '../assets/img/products/french-toast.jpg', name: 'French Toast', price: 150, reviews:  
       [
@@ -28,7 +28,7 @@ export class ProductService {
           {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
           {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
       ],
-      description: 'Product description'
+      description: 'Our french toast is a slice of brioche bread dipped in milk and eggs, then pan fried in butter goodness. Each serving gives you two slices of bread topped with butter, syrup, powdered sugar, and some of your favorite berries.'
   },
   {id: 4, photo: '../assets/img/products/four-cheese-flatbread.jpg', name: 'Four Cheese Flatbread', price: 130, reviews:  
       [
@@ -36,7 +36,7 @@ export class ProductService {
           {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
           {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
       ],
-      description: 'Product description'
+      description: 'Our four cheese flatbread is perfect all the cheese-lovers out there. This snack is made of pizza dough, topped with provolone, mozzarella, parmesan, and ricotta, oven-baked to perfection.'
   },
   {id: 5, photo: '../assets/img/products/cinnamon-roll.jpg', name: 'Cinnamon Roll', price: 120, reviews:  
       [
@@ -52,7 +52,7 @@ export class ProductService {
           {username: 'username2', date: 'April 20, 2021', content: 'Ok quality.', stars: 3},
           {username: 'username3', date: 'April 20, 2021', content: 'Do not buy!', stars: 1},    
       ],
-      description: 'Product description'
+      description: 'This scrumptious, satisfying cookie love will uplift your day—sweet white chocolate chips and chopped macademia nuts embedded in a thick, chewy goodness of our favorite dough make this an absolute must-have.'
   }
   ]
 
