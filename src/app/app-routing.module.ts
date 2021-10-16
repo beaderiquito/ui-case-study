35d1f9ae25d0970f13e9699c2ab7423ee71c2ed1
beaderiquito <beaderiquito@gmail.com>
2021-10-16 22:37:29 +0800

    Page Not Found added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 
@@ -10,7 +11,7 @@ const routes: Routes = [
   {path: 'products/:searchTerm', component: ProductListComponent}, //still not working properly
   {path: 'products/product-details/:product-id', component: ProductDetailComponent},
   {path: 'cart', component: CartComponent},
-
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
