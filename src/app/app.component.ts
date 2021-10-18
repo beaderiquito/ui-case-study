592e12c64577702adadfd9e312e2d8ba56175d5a
Bea Deriquito <beaderiquito@gmail.com>
2021-10-19 00:18:22 +0800

    created app-state.ts and imported cart store in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { CartItem } from './cart-item.model';
+import { AppState } from './store/app-state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-online-store';
+export class AppComponent implements OnInit{
+  cartItems!: Observable<CartItem[]>;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(){
+    this.cartItems = this.store.select(store => store.cart);
+  }
 }
